927098da9c1f1a432def07dfbaa2493d8855a3ec
mlandrey <in48semenov@yandex.ru>
2022-10-04 22:21:31 +0300

    Added find max bbox with max confidence in inference fasterrcnn

--- src/utils/detection_predict.py
+++ src/utils/detection_predict.py
@@ -29,23 +29,32 @@ class YoloInference:
         else:
             return None
 
+
 class FasterRCNNInference:
 
     def __init__(self):
         self.model = torch.load(detect_model_path['path_frcnn'], map_location='cpu')
         self.model.eval()
 
+    def _get_bbox_with_max_score(self, predict):
+        max_score = 0
+        for value in predict:
+            if max_score < value['scores'].cpu().detach().numpy()[0]:
+                bbox = value['boxes'].cpu().detach().numpy()[0]
+
+        return bbox[0], bbox[1], bbox[2], bbox[3]
+
     @torch.no_grad()
     def __call__(self, img: Image):
         img_tensor = transforms.ToTensor()(np.array(img))
         predict = self.model([img_tensor.to('cpu')])
         if len(predict[0]['boxes'].cpu().detach().numpy()) > 0:
-            predict = predict.sort(key=lambda dictionary: dictionary['score'])
-            predict = predict[0]['boxes'].cpu().detach().numpy()[0]
+            xmin, ymin, xmax, ymax = self._get_bbox_with_max_score(predict)
 
-            return predict[0], predict[1], predict[2], predict[3]
+            return xmin, ymin, xmax, ymax
         else:
             return None
+
 
 class MTCCNInference:
 
